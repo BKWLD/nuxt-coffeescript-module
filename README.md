bbd41b8ddc0c5f62dbd2a4270affcaf2a7cf12fb
Robert Reinhard <robert.reinhard@bukwild.com>
2018-07-13 12:39:04 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # nuxt-coffeescript-module
-Adds Coffeescript support to your Nuxt app
+
+Adds Coffeescript support to your Nuxt app.  This is based on the [official Nuxt example](https://github.com/nuxt/nuxt.js/blob/dev/examples/coffeescript/modules/coffeescript.js). 
+
+## Install
+
+1. `yarn add nuxt-coffeescript-module`
+
+2. In `nuxt.config.js`:
+
+```js
+{
+  modules: [ 'nuxt-coffeescript-module' ]
+}
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+// Based on:
+// https://github.com/nuxt/nuxt.js/blob/dev/examples/coffeescript/modules/coffeescript.js
+module.exports = function () {
+
+	// Add .coffee extension for store, middleware and more
+	this.nuxt.options.extensions.push('coffee')
+
+	// Extend build
+	this.extendBuild(config => {
+
+		// Add CoffeeScript loader with Babel transpiling using babelrc
+		config.module.rules.push({
+			test: /\.coffee$/,
+			loader: 'babel-loader!coffee-loader'
+		})
+
+		// Add CoffeeScript loader for vue files (with Babel)
+		for (let rule of config.module.rules) {
+			if (rule.loader === 'vue-loader') {
+				rule.options.loaders.coffee = {
+					test: /\.coffee$/,
+					loader: 'babel-loader!coffee-loader'
+				}
+			}
+		}
+
+		// Add .coffee extension in webpack resolve
+		if (config.resolve.extensions.indexOf('.coffee') === -1) {
+			config.resolve.extensions.push('.coffee')
+		}
+	})
+}
