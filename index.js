b8ae8e5b4cd25f8ba824806736f7fd4347666f4a
Robert Reinhard <robert.reinhard@bukwild.com>
2019-03-05 14:50:39 -0800

    Updating for Nuxt 2

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
 // Based on:
 // https://github.com/nuxt/nuxt.js/blob/dev/examples/coffeescript/modules/coffeescript.js
-module.exports = function () {
+export default function () {
 
 	// Add .coffee extension for store, middleware and more
 	this.nuxt.options.extensions.push('coffee')
 
-	// Extend build
-	this.extendBuild(config => {
+	// Add CoffeeScript loader
+	this.extendBuild((config) => {
 
-		// Add CoffeeScript loader with Babel
+		// Add CoffeeScript loader
 		config.module.rules.push({
 			test: /\.coffee$/,
 			loader: 'coffee-loader',
 			options: {
 				transpile: {
-					presets: ['vue-app'], // The Nuxt default
+					presets: ['@nuxt/babel-preset-app'], // The Nuxt 2 default
 					plugins: ['transform-object-rest-spread']
 				}
 			}
 		})
-
-		// Add CoffeeScript loader for vue files (with Babel)
-		for (let rule of config.module.rules) {
-			if (rule.loader === 'vue-loader') {
-				rule.options.loaders.coffee = {
-					test: /\.coffee$/,
-					loader: 'coffee-loader',
-					options: {
-						transpile: {
-							presets: ['vue-app'], // The Nuxt default
-							plugins: ['transform-object-rest-spread']
-						}
-					}
-				}
-			}
-		}
 
 		// Add .coffee extension in webpack resolve
 		if (config.resolve.extensions.indexOf('.coffee') === -1) {
